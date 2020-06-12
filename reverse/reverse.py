77080f7bc85f625744411e018c5db5c2c0eb6755
Ous Bayaa <bayaaous@gmail.com>
2020-06-12 10:53:54 -0700

    completed reverse - no stretch

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,12 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node is None:
+            return None
+        if node.get_next() ==  None:
+            self.head = node
+            self.head.next_node = prev
+            return
+        else:
+            self.reverse_list(node.get_next(), node) 
+            node.next_node = prev
